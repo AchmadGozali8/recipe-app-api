fc928c82e8e144eb15c808331c34d876f6ebabc9
AchmadGozali8 <achmadg6@gmail.com>
2019-11-16 18:10:35 +0700

    Added recipe models

--- app/core/models.py
+++ app/core/models.py
@@ -64,3 +64,22 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    #while use string ('Ingredient') to load other model we don't need order that model on above
+    #model who loaded. in this case we don't need to make Ingredient model above Recipe model
+    ingredients = models.ManyToManyField('Ingredient') 
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
