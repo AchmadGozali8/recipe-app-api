ac7980c63bb165cf86d8af40ae69d6f7af07be8f
AchmadGozali8 <achmadg6@gmail.com>
2019-11-20 14:47:50 +0700

    Added image field to recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,9 +1,19 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
 # this is the best practice to import settings file
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate filepath for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
 
 
 class UserManager(BaseUserManager):
@@ -80,6 +90,7 @@ class Recipe(models.Model):
     #model who loaded. in this case we don't need to make Ingredient model above Recipe model
     ingredients = models.ManyToManyField('Ingredient') 
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
